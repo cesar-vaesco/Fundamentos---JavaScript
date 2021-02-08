c7cc569ea1d43409a22ac51011a5f1f347e5f0b1
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-07 20:00:17 -0600

    Clases: métodos setters y getters

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -5,32 +5,47 @@ class Persona {
     nombre = '';
     codigo = '';
     frase = '';
-    
-    constructor( nombre= 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
+    comida = '';
+
+    constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     }
-
-    quienSoy(){
-        console.log(`Soy  ${ this.nombre } y mi identidad es ${ this.codigo}`);
+    // Set - establece un valor
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
     }
 
-    miFrase(){
-         /*-- Para poder poder usar un método dentro de otro método tambien es necesario usar el operador this--*/
+    // Get - recuperar un valor
+
+    get getComidaFavorita() {
+        return this.comida;
+    }
+
+    quienSoy() {
+        console.log(`Soy  ${this.nombre} y mi identidad es ${this.codigo}`);
+    }
+
+    miFrase() {
+        /*-- Para poder poder usar un método dentro de otro método tambien es necesario usar el operador this--*/
         this.quienSoy();
-        console.log(` ${this.codigo } dice: ${this.frase}`);
-        
+        console.log(` ${this.codigo} dice: ${this.frase}`);
+
     }
 }
 
-const spiderman = new Persona('Peter Parker', 'Spiderman','Soy tu amigable Vecino Spider Man ');
+const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable Vecino Spider Man ');
 console.log(spiderman);
-const ironman = new Persona('Toni Stark', 'Iron','Yo soy Ironman ');
+const ironman = new Persona('Toni Stark', 'Iron', 'Yo soy Ironman ');
 console.log(ironman);
 
 // Un método para ser invocado se requiere usar los parentesis, 
 // de no hacerlo, lo que se estaciendo es solamente usar su referencia 
-spiderman.quienSoy();
-ironman.quienSoy();
+
 spiderman.miFrase();
+
+//
+spiderman.setComidaFavorita = 'El pay de cereza de la tía May';
+
+console.log(`La comida favorita de ${spiderman.nombre} es '${spiderman.getComidaFavorita}'`);
